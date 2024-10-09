dd762b2ff0c946abe32bab5e028b8f6acf7e4df8
Ihliana <Ihnatco@protonmail.com>
2024-10-09 05:27:03 +0200

    Implementation of hasKey, values, entries

--- Data structures/Hash Tables/Tests/class-implementation.test.js
+++ Data structures/Hash Tables/Tests/class-implementation.test.js
@@ -4,7 +4,7 @@ describe("Hash Table implementation", () => {
   let hashTable;
 
   beforeEach(() => {
-    hashTable = new HashTable(50);
+    hashTable = new HashTable(137);
   });
 
   it("set method working properly", () => {
@@ -17,6 +17,13 @@ describe("Hash Table implementation", () => {
     expect(hashTable.get("occupation")).toBe("Engineer");
   });
 
+  it("should hash a key to a valid index", () => {
+    const index = hashTable._hash("testKey");
+
+    expect(index).toBeGreaterThanOrEqual(0);
+    expect(index).toBeLessThan(137);
+  });
+
   it("handling collision", () => {
     hashTable.set("abc", "Collision1");
     hashTable.set("cba", "Collision2");
@@ -25,12 +32,12 @@ describe("Hash Table implementation", () => {
     expect(hashTable.get("cba")).toBe("Collision2");
   });
 
-  it("shouldn't update a value if key exists", () => {
+  it("should update a value if key exists", () => {
     hashTable.set("key1", "value1");
     hashTable.set("key1", "value2");
 
     const index = hashTable._hash("key1");
-    expect(hashTable.data[index][0][1]).toBe("value1");
+    expect(hashTable.data[index][0][1]).toBe("value2");
   });
 
   it("should remove a value", () => {
@@ -40,12 +47,63 @@ describe("Hash Table implementation", () => {
     expect(hashTable.data[index]).toStrictEqual([]);
   });
 
+  it("returns true if contains a key", () => {
+    hashTable.set("key1", "value1");
+    hashTable.set("key2", "value2");
+
+    expect(hashTable.hasKey("key2")).toBe(true);
+  });
+
+  it("returns false if doesn't contain the key", () => {
+    hashTable.set("key1", "value1");
+    hashTable.set("key2", "value2");
+
+    expect(hashTable.hasKey("key3")).toBe(false);
+  });
+
   it("should return all keys", () => {
     hashTable.set("key1", "value1");
     hashTable.set("key2", "value2");
 
+    //Get the keys from the hash table
     const keys = hashTable.keys();
+
+    //Check if it contains the expected values
     expect(keys).toContain("key1");
     expect(keys).toContain("key2");
+  });
+
+  it("should return all values", () => {
+    // Add key-value pairs
+    hashTable.set("key1", "value1");
+    hashTable.set("key2", "value2");
+    hashTable.set("key3", "value3");
+
+    // Get the values from the hash table
+    const values = hashTable.values();
+
+    //Check if it contains the expected values
+    expect(values).toContain("value1");
+    expect(values).toContain("value2");
+    expect(values).toContain("value3");
+  });
+
+  it("should return undefined if empty", () => {
+    expect(hashTable.values()).toBe(undefined);
+  });
+
+  it("should return all entries", () => {
+    hashTable.set("key1", "value1");
+    hashTable.set("key2", "value2");
+
+    let entries = hashTable.entries();
+
+    expect(entries.length).toBe(2);
+    expect(entries[0]).toStrictEqual(["key1", "value1"]);
+    expect(entries[1]).toStrictEqual(["key2", "value2"]);
+  });
+
+  it("if no entries, undefind", () => {
+    expect(hashTable.entries()).toBe(undefined);
   });
 });
